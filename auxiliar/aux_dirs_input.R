cbd0f3d9674de8d4e5fa02123b1cfe60e3333181
Santiago Ortúzar <saortuza@uc.cl>
2024-07-11 20:16:23 -0400

    corregir directorio

--- auxiliar/aux_dirs_input.R
+++ auxiliar/aux_dirs_input.R
@@ -8,7 +8,7 @@ folder_usuario <- paste0("C:/Users/",
 folder_onedrive_casa <- paste0(folder_usuario,"/OneDrive")
 
 ### Carpeta con bases de datos (estadística descriptiva) ########################
-folder_data_descriptiva <- paste0(folder_onedrive_casa,"/Documentos/2022 Ayudantía UAH A. González Estadística Descriptiva en R/BBDD")
+folder_data_descriptiva <- paste0(folder_onedrive_casa,"/Documentos/2022 Ayudantía UAH Estadística Descriptiva R/BBDD")
 
 ###### BBDD Consumo Cultural ######
 file_data_consumo_cultural <- paste0(folder_data_descriptiva,"/consumo.cultural.Rdata")
