0a102c485571c879ec4cd79b5b95fbf88252ad8a
Santiago Ortuzar <saortuza@uc.cl>
2024-12-08 08:58:49 -0300

    incluir carga de tutorial de R

--- uah_ayudantia_estadistica_2022_masterfile.R
+++ uah_ayudantia_estadistica_2022_masterfile.R
@@ -15,8 +15,20 @@ tinytex::tlmgr_install(
       "float",
       "adjustbox",
       "xcolor"))
+library(quarto)
 
 # Correr diapositivas estadística descriptiva ----------------------------------
+### Correr tutoria instalación R ###############################################
+for(rmd in list.files(path=paste0(rprojroot::find_rstudio_root_file(),"/ayudantias_estadistica_descriptiva"),
+                      pattern="^tutorial\\_instalacion\\_R\\.Rmd$",
+                      full.names=TRUE,
+                      recursive=FALSE))
+{
+  rmarkdown::render(input=rmd) # sin especificar output_dir cada archivo se crea donde está su propio RMD
+}
+
+rm(list=ls())
+
 ### Correr diapositivas en loop ################################################
 for(rmd in list.files(path=paste0(rprojroot::find_rstudio_root_file(),"/ayudantias_estadistica_descriptiva"),
                       pattern="^presentacion\\_ayudantia\\_[0-1]{2}\\.Rmd$",
