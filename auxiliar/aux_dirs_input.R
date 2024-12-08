8bc4025e14e566e7652f832ac1657a4221e810e8
Santiago Ortuzar <saortuza@uc.cl>
2024-12-08 08:01:19 -0300

    mover ubicación de archivos y actualizar gitignore

--- auxiliar/aux_dirs_input.R
+++ auxiliar/aux_dirs_input.R
@@ -1,18 +1,18 @@
 
 # Directorios OneDrive ---------------------------------------------------------
 ### OneDrive ###################################################################
-folder_usuario <- paste0("C:/Users/",
+carpeta_usuario <- paste0("C:/Users/",
                          tolower(Sys.info()['user']))
 
 ### OneDrive ###################################################################
-folder_onedrive_casa <- paste0(folder_usuario,"/OneDrive")
+carpeta_onedrive_casa <- paste0(carpeta_usuario,"/OneDrive")
 
 ### Carpeta con bases de datos (estadística descriptiva) ########################
-folder_data_descriptiva <- paste0(folder_onedrive_casa,"/Documentos/2022 Ayudantía UAH Estadística Descriptiva R/BBDD")
+carpeta_data_descriptiva <- paste0(carpeta_onedrive_casa,"/Documentos/2022 Ayudantía UAH Estadística Descriptiva R/BBDD")
 
 ###### BBDD Consumo Cultural ######
-file_data_consumo_cultural <- paste0(folder_data_descriptiva,"/consumo.cultural.Rdata")
+archivo_data_consumo_cultural <- paste0(carpeta_data_descriptiva,"/consumo.cultural.Rdata")
 
 ### Carpeta con bases de datos (estadística inferencial) #######################
-folder_data_inferencia <- paste0(folder_onedrive_casa,"/Documentos/2022 Ayudantía UAH A. González Estadística Inferencial en R/BBDD")
+carpeta_data_inferencia <- paste0(carpeta_onedrive_casa,"/Documentos/2022 Ayudantía UAH A. González Estadística Inferencial en R/BBDD")
 
