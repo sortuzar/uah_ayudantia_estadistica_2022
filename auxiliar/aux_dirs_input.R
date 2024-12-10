8d94b3ec05980a2c3f8db2a528fd7649e14e1c1f
Santiago Ortuzar <saortuza@uc.cl>
2024-12-10 11:45:33 -0300

    corregir directorios de onedrive

--- auxiliar/aux_dirs_input.R
+++ auxiliar/aux_dirs_input.R
@@ -8,11 +8,12 @@ carpeta_usuario <- paste0("C:/Users/",
 carpeta_onedrive_casa <- paste0(carpeta_usuario,"/OneDrive")
 
 ### Carpeta con bases de datos (estadística descriptiva) ########################
-carpeta_data_descriptiva <- paste0(carpeta_onedrive_casa,"/Documentos/2022 Ayudantía UAH Estadística Descriptiva R/BBDD")
+carpeta_onedrive_descriptiva <- paste0(carpeta_onedrive_casa,"/Documentos/2022 Ayudantía UAH Estadística Descriptiva R")
+carpeta_data_descriptiva <- paste0(carpeta_onedrive_descriptiva,"/BBDD")
 
 ###### BBDD Consumo Cultural ######
 archivo_data_consumo_cultural <- paste0(carpeta_data_descriptiva,"/consumo.cultural.Rdata")
 
 ### Carpeta con bases de datos (estadística inferencial) #######################
-carpeta_data_inferencia <- paste0(carpeta_onedrive_casa,"/Documentos/2022 Ayudantía UAH A. González Estadística Inferencial en R/BBDD")
+carpeta_onedrive_inferencia <- paste0(carpeta_onedrive_casa,"/Documentos/2022 Ayudantía UAH Estadística Inferencial R")
 
