453ccc6648c141d1d6f77f1b0a9cf65c9b00bb75
Santiago Ortúzar <sa.ortuzar@gmail.com>
2025-07-22 16:50:41 -0400

    aplicar style_dir()

--- ayudantias_estadistica_descriptiva/ayudantia05/material_ayudantia_05.R
+++ ayudantias_estadistica_descriptiva/ayudantia05/material_ayudantia_05.R
@@ -1,113 +1,126 @@
+#### Ayudant?a 5####
+#### Repaso y ejercicios####
+#### Viernes 13 de mayo de 2022####
+#### Ayudante Santiago Ort?zar####
+#### Estad?stica descriptiva-R, FEN-UAH####
+#### Prof. Alejandro Gonz?lez####
 
-####Ayudantía 5####
-####Repaso y ejercicios####
-####Viernes 13 de mayo de 2022####
-####Ayudante Santiago Ortúzar####
-####Estadística descriptiva-R, FEN-UAH####
-####Prof. Alejandro González####
+#### Ayudant?as que nos quedan:
 
-####Ayudantías que nos quedan:
+######## Ayudant?a 6: Viernes 20 de mayo (c?psula).
 
-########Ayudantía 6: Viernes 20 de mayo (cápsula).
+######## Ayudant?a 7: Viernes 27 de mayo, 19.00-20.20 (sincr?nica).****
 
-########Ayudantía 7: Viernes 27 de mayo, 19.00-20.20 (sincrónica).****
+######## Ayudant?a 8: Viernes 3 de junio (c?psula).
 
-########Ayudantía 8: Viernes 3 de junio (cápsula).
-
-########MÁS: Ayudantías particulares que agendemos
-
+######## M?S: Ayudant?as particulares que agendemos
 
 
-####Siempre limpiar el environment####
-rm(list=ls())
-####Buscar el directorio desde el que abrimos el script####
+
+#### Siempre limpiar el environment####
+rm(list = ls())
+#### Buscar el directorio desde el que abrimos el script####
 getwd()
-####Fijar el directorio en el que queremos trabajar####
-setwd("D:/SANTIAGO/OneDrive/Documentos/2022 Ayudantía UAH A. González Estadística R")
+#### Fijar el directorio en el que queremos trabajar####
+setwd("D:/SANTIAGO/OneDrive/Documentos/2022 Ayudant?a UAH A. Gonz?lez Estad?stica R")
 
-#Datos
+# Datos
 data(swiss)
 
-####1####
-ayudantia5 <- swiss[ , c("Fertility", "Examination", 
-                         "Education", "Catholic")]
+#### 1####
+ayudantia5 <- swiss[, c(
+  "Fertility", "Examination",
+  "Education", "Catholic"
+)]
 
-####2####
+#### 2####
 rm(swiss)
 
-####2####
-#Primer paso: crear una columna "vacía" (todas las filas = 0)
-ayudantia5$religion <- rep(0,nrow(ayudantia5))
+#### 2####
+# Primer paso: crear una columna "vac?a" (todas las filas = 0)
+ayudantia5$religion <- rep(0, nrow(ayudantia5))
 
-#Segundo paso: 
-ayudantia5$religion[ayudantia5$Catholic<20] <- 1
-ayudantia5$religion[ayudantia5$Catholic>=20&ayudantia5$Catholic<=80] <- 2
-ayudantia5$religion[ayudantia5$Catholic>80] <- 3
+# Segundo paso:
+ayudantia5$religion[ayudantia5$Catholic < 20] <- 1
+ayudantia5$religion[ayudantia5$Catholic >= 20 & ayudantia5$Catholic <= 80] <- 2
+ayudantia5$religion[ayudantia5$Catholic > 80] <- 3
 
-#Tercer paso: factor
-ayudantia5$religion <- factor(ayudantia5$religion, 
-                              levels=c(1,2,3),
-                              labels=c("Provincia protestante",
-                                       "Provincia integrada",
-                                       "Provincia católica"))
+# Tercer paso: factor
+ayudantia5$religion <- factor(ayudantia5$religion,
+  levels = c(1, 2, 3),
+  labels = c(
+    "Provincia protestante",
+    "Provincia integrada",
+    "Provincia cat?lica"
+  )
+)
 
-####3####
-#¿Cuántas provincias hay de cada tipo? Tabla de frecuencias y de proporciones
+#### 3####
+# ?Cu?ntas provincias hay de cada tipo? Tabla de frecuencias y de proporciones
 table(ayudantia5$religion)
 
 prop.table(table(ayudantia5$religion))
-100*prop.table(table(ayudantia5$religion)) #de proporciones a porcentajes
-round(100*prop.table(table(ayudantia5$religion)), digits=1) #redondear porcentaje
+100 * prop.table(table(ayudantia5$religion)) # de proporciones a porcentajes
+round(100 * prop.table(table(ayudantia5$religion)), digits = 1) # redondear porcentaje
 
-####4####
-#Courtelary, fila 1:
-(15+12)/2
-(ayudantia5$Examination[1]+ayudantia5$Education[1])/2
+#### 4####
+# Courtelary, fila 1:
+(15 + 12) / 2
+(ayudantia5$Examination[1] + ayudantia5$Education[1]) / 2
 
-#Delemont, fila 2:
-(6+9)/2
-(ayudantia5$Examination[2]+ayudantia5$Education[2])/2
+# Delemont, fila 2:
+(6 + 9) / 2
+(ayudantia5$Examination[2] + ayudantia5$Education[2]) / 2
 
-#Para todas las filas
-#Primer paso: crear una columna "vacía" (todas las filas = 0)
-ayudantia5$nivel.educativo <- rep(0,nrow(ayudantia5))
+# Para todas las filas
+# Primer paso: crear una columna "vac?a" (todas las filas = 0)
+ayudantia5$nivel.educativo <- rep(0, nrow(ayudantia5))
 
-ayudantia5$nivel.educativo <- (ayudantia5$Examination+ayudantia5$Education)/2
+ayudantia5$nivel.educativo <- (ayudantia5$Examination + ayudantia5$Education) / 2
 
-####5####
-ayudantia5.reducido <- ayudantia5[ , c("Fertility", "nivel.educativo",
-                                       "religion")]
+#### 5####
+ayudantia5.reducido <- ayudantia5[, c(
+  "Fertility", "nivel.educativo",
+  "religion"
+)]
 
-####6####
+#### 6####
 rm(ayudantia5)
 
-####7####
+#### 7####
 mean(ayudantia5.reducido$Fertility)
-tapply(ayudantia5.reducido$Fertility,
-       ayudantia5.reducido$religion,
-       mean)
+tapply(
+  ayudantia5.reducido$Fertility,
+  ayudantia5.reducido$religion,
+  mean
+)
 
 mean(ayudantia5.reducido$nivel.educativo)
-tapply(ayudantia5.reducido$nivel.educativo,
-       ayudantia5.reducido$religion,
-       mean)
+tapply(
+  ayudantia5.reducido$nivel.educativo,
+  ayudantia5.reducido$religion,
+  mean
+)
 
-####8A: Fertilidad####
-#boxplot para cada grupo
-#mediana para cada grupo
+#### 8A: Fertilidad####
+# boxplot para cada grupo
+# mediana para cada grupo
 
-boxplot(ayudantia5.reducido$Fertility ~ 
-          ayudantia5.reducido$religion)
+boxplot(ayudantia5.reducido$Fertility ~
+  ayudantia5.reducido$religion)
 
-tapply(ayudantia5.reducido$Fertility, 
-       ayudantia5.reducido$religion,
-       median)
+tapply(
+  ayudantia5.reducido$Fertility,
+  ayudantia5.reducido$religion,
+  median
+)
 
-####8B: Nivel educativo####
-boxplot(ayudantia5.reducido$nivel.educativo ~ 
-          ayudantia5.reducido$religion)
+#### 8B: Nivel educativo####
+boxplot(ayudantia5.reducido$nivel.educativo ~
+  ayudantia5.reducido$religion)
 
-tapply(ayudantia5.reducido$Fertility, 
-       ayudantia5.reducido$religion,
-       median)
-
+tapply(
+  ayudantia5.reducido$Fertility,
+  ayudantia5.reducido$religion,
+  median
+)
--- ayudantias_estadistica_descriptiva/ayudantia08/material_ayudantia_08.R
+++ ayudantias_estadistica_descriptiva/ayudantia08/material_ayudantia_08.R
@@ -1,199 +1,232 @@
+#### Ayudant?a 8####
+#### Repaso examen####
+#### Viernes 2 de junio de 2022####
+#### Ayudante Santiago Ort?zar####
+#### Estad?stica descriptiva-R, FEN-UAH####
+#### Prof. Alejandro Gonz?lez####
 
-####Ayudant?a 8####
-####Repaso examen####
-####Viernes 2 de junio de 2022####
-####Ayudante Santiago Ort?zar####
-####Estad?stica descriptiva-R, FEN-UAH####
-####Prof. Alejandro Gonz?lez####
-
-####Siempre limpiar el environment####
-rm(list=ls())
-####Buscar el directorio desde el que abrimos el script####
+#### Siempre limpiar el environment####
+rm(list = ls())
+#### Buscar el directorio desde el que abrimos el script####
 getwd()
-####Fijar el directorio en el que queremos trabajar####
+#### Fijar el directorio en el que queremos trabajar####
 setwd("D:/SANTIAGO/OneDrive/Documentos/2022 Ayudantía UAH A. González Estadística R 1 Descriptiva")
-####Desactivar notaci?n cient?fica####
-options(scipen=999)
+#### Desactivar notaci?n cient?fica####
+options(scipen = 999)
 
 
-####Librer?as####
+#### Librer?as####
 library(dplyr)
 library(PASWR2)
 library(ggplot2)
 library(gridExtra)
 
-####1####
+#### 1####
 data(starwars)
 
-####1a####
-hist(starwars$birth_year, 
-     xlim=c(0,900),
-     xaxp=c(0,900,5),
-     ylim=c(0,40),
-     main="Edad de los personajes de Star Wars",
-     xlab="Edad (a?os)",
-     ylab="Recuento de frecuencias")
+#### 1a####
+hist(starwars$birth_year,
+  xlim = c(0, 900),
+  xaxp = c(0, 900, 5),
+  ylim = c(0, 40),
+  main = "Edad de los personajes de Star Wars",
+  xlab = "Edad (a?os)",
+  ylab = "Recuento de frecuencias"
+)
 
-ggplot(starwars, aes(x=birth_year)) + 
-  geom_histogram(bins=7) + #ajustar los bins cambia la apariencia
+ggplot(starwars, aes(x = birth_year)) +
+  geom_histogram(bins = 7) + # ajustar los bins cambia la apariencia
   scale_x_continuous(breaks = seq(0, 900, by = 180)) +
-  ylim(0,40) +
-  labs(title="Edad de los personajes de Star Wars",
-         x="Edad (a?os)",
-         y="Recuento de frecuencias")
+  ylim(0, 40) +
+  labs(
+    title = "Edad de los personajes de Star Wars",
+    x = "Edad (a?os)",
+    y = "Recuento de frecuencias"
+  )
 
-####1b####
-cor(starwars$birth_year,starwars$mass,use="complete.obs")
-#Coeficiente de correlaci?n = 0.48
+#### 1b####
+cor(starwars$birth_year, starwars$mass, use = "complete.obs")
+# Coeficiente de correlaci?n = 0.48
 
-ggplot(starwars, aes(x=birth_year,y=mass)) +
+ggplot(starwars, aes(x = birth_year, y = mass)) +
   geom_point() +
-  scale_x_continuous(breaks=seq(0,900, 360)) +
-  scale_y_continuous(breaks=seq(0,1400,240)) +
+  scale_x_continuous(breaks = seq(0, 900, 360)) +
+  scale_y_continuous(breaks = seq(0, 1400, 240)) +
   theme_bw() +
-  labs(title="Correlaci?n entre edad y peso de los personajes de Star Wars",
-       x="Edad (a?os)",
-       y="Peso (kilos)")
+  labs(
+    title = "Correlaci?n entre edad y peso de los personajes de Star Wars",
+    x = "Edad (a?os)",
+    y = "Peso (kilos)"
+  )
 
-####1c####
-histograma.edad <- ggplot(starwars, aes(x=birth_year)) + 
-  geom_histogram(bins=7) + #ajustar los bins cambia la apariencia
+#### 1c####
+histograma.edad <- ggplot(starwars, aes(x = birth_year)) +
+  geom_histogram(bins = 7) + # ajustar los bins cambia la apariencia
   scale_x_continuous(breaks = seq(0, 900, by = 180)) +
-  ylim(0,40) +
-  labs(title="Edad de los personajes de Star Wars",
-       x="Edad (a?os)",
-       y="Recuento de frecuencias")
+  ylim(0, 40) +
+  labs(
+    title = "Edad de los personajes de Star Wars",
+    x = "Edad (a?os)",
+    y = "Recuento de frecuencias"
+  )
 
-dispersion.edad.peso <- ggplot(starwars, aes(x=birth_year,y=mass)) +
+dispersion.edad.peso <- ggplot(starwars, aes(x = birth_year, y = mass)) +
   geom_point() +
-  scale_x_continuous(breaks=seq(0,900, 360)) +
-  scale_y_continuous(breaks=seq(0,1400,240)) +
+  scale_x_continuous(breaks = seq(0, 900, 360)) +
+  scale_y_continuous(breaks = seq(0, 1400, 240)) +
   theme_bw() +
-  labs(title="Correlaci?n entre edad y peso de los personajes de Star Wars",
-       x="Edad (a?os)",
-       y="Peso (kilos)")
+  labs(
+    title = "Correlaci?n entre edad y peso de los personajes de Star Wars",
+    x = "Edad (a?os)",
+    y = "Peso (kilos)"
+  )
 
 graficos.starwars <- grid.arrange(histograma.edad, dispersion.edad.peso,
-                                  nrow=1,ncol=2)
+  nrow = 1, ncol = 2
+)
 
 ggsave("material_ayudantia_08_graficos-starwars.jpg", graficos.starwars,
-       width=7,
-       height=7)
+  width = 7,
+  height = 7
+)
 
-####2####
+#### 2####
 data(swiss)
 
-####2a####
-swiss.examination.fertility <- ggplot(swiss, aes(x=Examination,y=Fertility)) +
+#### 2a####
+swiss.examination.fertility <- ggplot(swiss, aes(x = Examination, y = Fertility)) +
   geom_point() +
-  scale_x_continuous(breaks=seq(0,40, 10)) +
-  scale_y_continuous(breaks=seq(30,100,10)) +
+  scale_x_continuous(breaks = seq(0, 40, 10)) +
+  scale_y_continuous(breaks = seq(30, 100, 10)) +
   theme_bw() +
-  labs(title="Correlaci?n entre puntaje m?ximo y fertilidad",
-       x="Calificaci?n m?xima (porcentaje)",
-       y="Fertilidad (tasa de nacimientos)") +
-  geom_smooth(method="lm", se=FALSE)
+  labs(
+    title = "Correlaci?n entre puntaje m?ximo y fertilidad",
+    x = "Calificaci?n m?xima (porcentaje)",
+    y = "Fertilidad (tasa de nacimientos)"
+  ) +
+  geom_smooth(method = "lm", se = FALSE)
 swiss.examination.fertility
 
-swiss.education.fertility <- ggplot(swiss, aes(x=Education,y=Fertility)) +
+swiss.education.fertility <- ggplot(swiss, aes(x = Education, y = Fertility)) +
   geom_point() +
-  scale_x_continuous(breaks=seq(0,60, 10)) +
-  scale_y_continuous(breaks=seq(30,100,10)) +
+  scale_x_continuous(breaks = seq(0, 60, 10)) +
+  scale_y_continuous(breaks = seq(30, 100, 10)) +
   theme_bw() +
-  labs(title="Correlaci?n entre proporci?n de la poblaci?n con b?sica completa y fertilidad",
-       x="Educaci?n b?sica completa (porcentaje)",
-       y="Fertilidad (tasa de nacimientos)") +
-  geom_smooth(method="lm", se=FALSE)
+  labs(
+    title = "Correlaci?n entre proporci?n de la poblaci?n con b?sica completa y fertilidad",
+    x = "Educaci?n b?sica completa (porcentaje)",
+    y = "Fertilidad (tasa de nacimientos)"
+  ) +
+  geom_smooth(method = "lm", se = FALSE)
 swiss.education.fertility
 
-swiss.education.examination <- ggplot(swiss, aes(x=Education,y=Examination)) +
+swiss.education.examination <- ggplot(swiss, aes(x = Education, y = Examination)) +
   geom_point() +
-  scale_x_continuous(breaks=seq(0,60, 10)) +
-  scale_y_continuous(breaks=seq(0,40,10)) +
+  scale_x_continuous(breaks = seq(0, 60, 10)) +
+  scale_y_continuous(breaks = seq(0, 40, 10)) +
   theme_bw() +
-  labs(title="Correlaci?n entre proporci?n de la poblaci?n con b?sica completa y calificaci?n m?xima",
-       x="Educaci?n b?sica completa (porcentaje)",
-       y="Calificaci?n m?xima (porcentaje)") +
-  geom_smooth(method="lm", se=FALSE)
+  labs(
+    title = "Correlaci?n entre proporci?n de la poblaci?n con b?sica completa y calificaci?n m?xima",
+    x = "Educaci?n b?sica completa (porcentaje)",
+    y = "Calificaci?n m?xima (porcentaje)"
+  ) +
+  geom_smooth(method = "lm", se = FALSE)
 swiss.education.examination
 
-####2b####
-cor(swiss$Examination,swiss$Fertility,use="complete.obs")
+#### 2b####
+cor(swiss$Examination, swiss$Fertility, use = "complete.obs")
 #-0.65
 
-cor(swiss$Education,swiss$Fertility,use="complete.obs")
+cor(swiss$Education, swiss$Fertility, use = "complete.obs")
 #-0.66
 
-cor(swiss$Education,swiss$Examination,use="complete.obs")
-#0.70
+cor(swiss$Education, swiss$Examination, use = "complete.obs")
+# 0.70
 
-####2c####
+#### 2c####
 grid.arrange(swiss.education.examination,
-             swiss.education.fertility,
-             swiss.examination.fertility,
-             nrow=3,
-             ncol=1)
+  swiss.education.fertility,
+  swiss.examination.fertility,
+  nrow = 3,
+  ncol = 1
+)
 
-####3####
+#### 3####
 data(EPIDURALF)
 
-####3a####
-ease.treatment <- xtabs(~ease+treatment,data=EPIDURALF)
+#### 3a####
+ease.treatment <- xtabs(~ ease + treatment, data = EPIDURALF)
 prop.table(ease.treatment, 1)
 
-ease.doctor <- xtabs(~ease+doctor,data=EPIDURALF)
+ease.doctor <- xtabs(~ ease + doctor, data = EPIDURALF)
 prop.table(ease.doctor, 1)
 
-####3b####
-ggplot(EPIDURALF, 
-       aes(x = treatment, 
-           fill = ease)) + 
+#### 3b####
+ggplot(
+  EPIDURALF,
+  aes(
+    x = treatment,
+    fill = ease
+  )
+) +
   geom_bar(position = "stack")
-#Versi?n transpuesta:
-ggplot(EPIDURALF, 
-       aes(x = ease, 
-           fill = treatment)) + 
-  geom_bar(position = "stack")
-
-
-ggplot(EPIDURALF, 
-       aes(x = doctor, 
-           fill = ease)) + 
-  geom_bar(position = "stack")
-#Versi?n transpuesta:
-ggplot(EPIDURALF, 
-       aes(x = ease, 
-           fill = doctor)) + 
+# Versi?n transpuesta:
+ggplot(
+  EPIDURALF,
+  aes(
+    x = ease,
+    fill = treatment
+  )
+) +
   geom_bar(position = "stack")
 
-####3c####
-cor(EPIDURALF$cm,EPIDURALF$kg,use="complete.obs")
-#0.37
 
-####4####
+ggplot(
+  EPIDURALF,
+  aes(
+    x = doctor,
+    fill = ease
+  )
+) +
+  geom_bar(position = "stack")
+# Versi?n transpuesta:
+ggplot(
+  EPIDURALF,
+  aes(
+    x = ease,
+    fill = doctor
+  )
+) +
+  geom_bar(position = "stack")
 
-#Media:
-mean(starwars$birth_year, na.rm=TRUE)
+#### 3c####
+cor(EPIDURALF$cm, EPIDURALF$kg, use = "complete.obs")
+# 0.37
 
-#Mediana:
-median(starwars$birth_year, na.rm=TRUE)
+#### 4####
 
-#Cuartiles:
-quantile(starwars$birth_year, probs=seq(0,1,0.25), na.rm=TRUE)
+# Media:
+mean(starwars$birth_year, na.rm = TRUE)
 
-#Deciles:
-quantile(starwars$birth_year, probs=seq(0,1,0.10), na.rm=TRUE)
+# Mediana:
+median(starwars$birth_year, na.rm = TRUE)
 
-#Percentiles:
-quantile(starwars$birth_year, probs=seq(0,1,0.01), na.rm=TRUE)
+# Cuartiles:
+quantile(starwars$birth_year, probs = seq(0, 1, 0.25), na.rm = TRUE)
 
-#Desviaci?n est?ndar:
-sd(starwars$birth_year, na.rm=TRUE)
+# Deciles:
+quantile(starwars$birth_year, probs = seq(0, 1, 0.10), na.rm = TRUE)
 
-#Varianza:
-var(starwars$birth_year, na.rm=TRUE)
+# Percentiles:
+quantile(starwars$birth_year, probs = seq(0, 1, 0.01), na.rm = TRUE)
 
-##Recuerde: la ra?z cuadrada de la varianza es la desviaci?n est?ndar
-sqrt(var(starwars$birth_year, na.rm=TRUE))==sd(starwars$birth_year, na.rm=TRUE)
+# Desviaci?n est?ndar:
+sd(starwars$birth_year, na.rm = TRUE)
 
-#Que la Fuerza los acompa?e
+# Varianza:
+var(starwars$birth_year, na.rm = TRUE)
+
+## Recuerde: la ra?z cuadrada de la varianza es la desviaci?n est?ndar
+sqrt(var(starwars$birth_year, na.rm = TRUE)) == sd(starwars$birth_year, na.rm = TRUE)
+
+# Que la Fuerza los acompa?e
